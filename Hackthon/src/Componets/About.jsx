9bee928879eb98ac60c5d57b6ce96beb64230e80
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2025-01-02 21:21:17 +0530

    Add GHR logo to Home component and adjust About section heading size

--- Hackthon/src/Componets/About.jsx
+++ Hackthon/src/Componets/About.jsx
@@ -4,7 +4,7 @@ import "../Styles/about.css";
 const AboutSection = () => {
   return (
     <div className="">
-      <h1 className="text-center font-[anzo5] uppercase leading-[1] text-[25vw] text-purple-700">About Us</h1>
+      <h1 className="text-center font-[anzo5] uppercase leading-[1] text-[2vw] text-purple-700">About Us</h1>
       <div className="text-center h-full w-[80vw]  rounded-2xl justify-center items-center mx-auto p-5">
         <h2 className="text-white font-[anzo2]"> 
           Embarking on the next chapter of an exhilarating legacy, we present GHR-HACK-1.0. This ultimate blend of coders, innovators, designers, and tech-connoisseurs is a celebration of expertise and creativity that promises an unparalleled experience. Get ready to embark on an exciting journey, overcoming challenges with innovative solutions, and creating a lasting impact. The event is a mega-fest of hackers racing against time for intensive development. This edition promises to redefine the boundaries of what is possible in hacking, where grandeur meets innovation. So, gear up for a roller-coaster ride of creativity, innovation, and technology!
--- Hackthon/src/Componets/Home.jsx
+++ Hackthon/src/Componets/Home.jsx
@@ -1,9 +1,11 @@
-
+import 
 
 const Home = () => {
   return (
     <div className="mt-10">
-        <div className="bg-black bg-opacity-45"></div>
+        <div className="bg-black bg-opacity-45">
+            <img src="Hackthon\src\assets\images\GHR.png" alt="" />
+        </div>
       
     </div>
   )
