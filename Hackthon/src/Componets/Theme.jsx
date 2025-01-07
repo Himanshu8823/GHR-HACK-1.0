5cdb848117c4cc796ae2c812b46960847cd21874
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2025-01-07 22:52:29 +0530

    Refactor About, Box, and Theme components: update styles for improved layout, enhance visual consistency, and add new headings

--- Hackthon/src/Componets/Theme.jsx
+++ Hackthon/src/Componets/Theme.jsx
@@ -40,12 +40,12 @@ const HackathonThemes = () => {
     return (
         <section className=" bg-[#014EB6] text-white py-16">
             <div  className="container mx-12 px-12">
-                <h2 className="text-4xl font-bold mb-12 text-center text-white">Hackathon Themes</h2>
+                <h2 className="font-[Ghr4] text-5xl mb-12 text-center text-white">Hackathon Themes</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                     {themes.map((theme, index) => (
                         <div
                             key={index}
-                            className="theme-card bg-gradient-to-b from-teal-600 to-teal-800 rounded-lg p-6 flex flex-col items-center shadow-lg transition-transform transform hover:scale-105"
+                            className="theme-card h-60 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg p-6 flex flex-col items-center shadow-lg transition-transform transform hover:scale-105"
                         >
                             <img
                                 src={theme.image}
